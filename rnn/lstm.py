879a961361d4a07ebf3fbd2a752b4cd6e61aee62
saraansh <bsaraansh@gmail.com>
2018-05-09 11:11:47 +0530

    Processing char-by-char RNN on LSTM Cell

---
+++ rnn/lstm.py
@@ -0,0 +1,22 @@
+import sys
+from keras.callbacks import ModelCheckpoint
+
+from utils import getDataset, defineModel, getText
+
+def train(filename):
+  raw_text = getText(filename)
+  data = getDataset(raw_text)
+  model = defineModel(data)
+  fitModel(model, data)
+
+def fitModel(model, data):
+  filepath="weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+  checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
+  callbacks_list = [checkpoint]
+
+  # fit the model
+  model.fit(data['input'], data['output'], nb_epoch=200, batch_size=64, callbacks=callbacks_list)
+    
+if __name__ == "__main__":
+    #filename = sys.argv[1];
+    train('C:/Users/Hopeless/ML Projects/TypeAI/metrics_gui/curated_v1.txt')
